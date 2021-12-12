8ac120aae86d423cb905f720c5213ff192124c3c
IshwaryaAshok <ishwarya125ece@gmail.com>
2021-12-12 14:25:14 -0800

    Renamed main file to IBoot.c

--- IBoot/IBootLPC55s69_hello_world/Debug/board/clock_config.d
+++ IBoot/IBootLPC55s69_hello_world/Debug/board/clock_config.d
@@ -1,35 +1,35 @@
 board/clock_config.o board/clock_config.d: ../board/clock_config.c \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\LPC55S69\drivers/fsl_power.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\drivers/fsl_common.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/fsl_device_registers.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/LPC55S69_cm33_core0.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/core_cm33.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/cmsis_version.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/cmsis_compiler.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/cmsis_gcc.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/mpu_armv8.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/system_LPC55S69_cm33_core0.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/LPC55S69_cm33_core0_features.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\drivers/fsl_common_arm.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\drivers/fsl_clock.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\drivers/fsl_common.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\drivers/fsl_reset.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\drivers/fsl_clock.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\LPC55S69\drivers/fsl_power.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\drivers/fsl_common.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\device/fsl_device_registers.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\device/LPC55S69_cm33_core0.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\CMSIS/core_cm33.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\CMSIS/cmsis_version.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\CMSIS/cmsis_compiler.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\CMSIS/cmsis_gcc.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\CMSIS/mpu_armv8.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\device/system_LPC55S69_cm33_core0.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\device/LPC55S69_cm33_core0_features.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\drivers/fsl_common_arm.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\drivers/fsl_clock.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\drivers/fsl_common.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\drivers/fsl_reset.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\drivers/fsl_clock.h \
  ../board/clock_config.h
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\LPC55S69\drivers/fsl_power.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\drivers/fsl_common.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/fsl_device_registers.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/LPC55S69_cm33_core0.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/core_cm33.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/cmsis_version.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/cmsis_compiler.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/cmsis_gcc.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/mpu_armv8.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/system_LPC55S69_cm33_core0.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/LPC55S69_cm33_core0_features.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\drivers/fsl_common_arm.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\drivers/fsl_clock.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\drivers/fsl_common.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\drivers/fsl_reset.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\drivers/fsl_clock.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\LPC55S69\drivers/fsl_power.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\drivers/fsl_common.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\device/fsl_device_registers.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\device/LPC55S69_cm33_core0.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\CMSIS/core_cm33.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\CMSIS/cmsis_version.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\CMSIS/cmsis_compiler.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\CMSIS/cmsis_gcc.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\CMSIS/mpu_armv8.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\device/system_LPC55S69_cm33_core0.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\device/LPC55S69_cm33_core0_features.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\drivers/fsl_common_arm.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\drivers/fsl_clock.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\drivers/fsl_common.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\drivers/fsl_reset.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\drivers/fsl_clock.h:
 ../board/clock_config.h:
--- IBoot/IBootLPC55s69_hello_world/Debug/drivers/fsl_usart.d
+++ IBoot/IBootLPC55s69_hello_world/Debug/drivers/fsl_usart.d
@@ -1,27 +1,27 @@
 drivers/fsl_usart.o drivers/fsl_usart.d: ../drivers/fsl_usart.c \
  ../drivers/fsl_usart.h ../drivers/fsl_common.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/fsl_device_registers.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/LPC55S69_cm33_core0.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/core_cm33.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/cmsis_version.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/cmsis_compiler.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/cmsis_gcc.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/mpu_armv8.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/system_LPC55S69_cm33_core0.h \
- E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/LPC55S69_cm33_core0_features.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\device/fsl_device_registers.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\device/LPC55S69_cm33_core0.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\CMSIS/core_cm33.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\CMSIS/cmsis_version.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\CMSIS/cmsis_compiler.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\CMSIS/cmsis_gcc.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\CMSIS/mpu_armv8.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\device/system_LPC55S69_cm33_core0.h \
+ E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\device/LPC55S69_cm33_core0_features.h \
  ../drivers/fsl_common_arm.h ../drivers/fsl_clock.h \
  ../drivers/fsl_reset.h ../drivers/fsl_flexcomm.h
 ../drivers/fsl_usart.h:
 ../drivers/fsl_common.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/fsl_device_registers.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/LPC55S69_cm33_core0.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/core_cm33.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/cmsis_version.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/cmsis_compiler.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/cmsis_gcc.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/mpu_armv8.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/system_LPC55S69_cm33_core0.h:
-E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/LPC55S69_cm33_core0_features.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\device/fsl_device_registers.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\device/LPC55S69_cm33_core0.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\CMSIS/core_cm33.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\CMSIS/cmsis_version.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\CMSIS/cmsis_compiler.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\CMSIS/cmsis_gcc.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\CMSIS/mpu_armv8.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\device/system_LPC55S69_cm33_core0.h:
+E\:\Ishwarya\IBootRepo\IBoot\IBootLPC55s69_hello_world\device/LPC55S69_cm33_core0_features.h:
 ../drivers/fsl_common_arm.h:
 ../drivers/fsl_clock.h:
 ../drivers/fsl_reset.h:
