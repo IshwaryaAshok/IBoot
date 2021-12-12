ab3850c894fa75d4bf320e4a57bb1668113486bd
IshwaryaAshok <ishwarya125ece@gmail.com>
2021-12-12 14:12:59 -0800

    Revert "Renamed IBoot main file and project name"
    This reverts commit bb2bbc157bfb998b7daa57dc35554e104fe94725.

---
+++ IBoot/IBootLPC55s69_hello_world/Debug/LPC55S69/drivers/fsl_power.d
@@ -0,0 +1,32 @@
+LPC55S69/drivers/fsl_power.o LPC55S69/drivers/fsl_power.d: \
+ ../LPC55S69/drivers/fsl_power.c \
+ E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\drivers/fsl_common.h \
+ E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/fsl_device_registers.h \
+ E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/LPC55S69_cm33_core0.h \
+ E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/core_cm33.h \
+ E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/cmsis_version.h \
+ E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/cmsis_compiler.h \
+ E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/cmsis_gcc.h \
+ E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/mpu_armv8.h \
+ E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/system_LPC55S69_cm33_core0.h \
+ E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/LPC55S69_cm33_core0_features.h \
+ E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\drivers/fsl_common_arm.h \
+ E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\drivers/fsl_clock.h \
+ E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\drivers/fsl_common.h \
+ E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\drivers/fsl_reset.h \
+ ../LPC55S69/drivers/fsl_power.h
+E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\drivers/fsl_common.h:
+E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/fsl_device_registers.h:
+E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/LPC55S69_cm33_core0.h:
+E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/core_cm33.h:
+E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/cmsis_version.h:
+E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/cmsis_compiler.h:
+E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/cmsis_gcc.h:
+E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\CMSIS/mpu_armv8.h:
+E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/system_LPC55S69_cm33_core0.h:
+E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\device/LPC55S69_cm33_core0_features.h:
+E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\drivers/fsl_common_arm.h:
+E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\drivers/fsl_clock.h:
+E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\drivers/fsl_common.h:
+E\:\Ishwarya\IBootRepo\IBootLPC55s69_hello_world\drivers/fsl_reset.h:
+../LPC55S69/drivers/fsl_power.h:
